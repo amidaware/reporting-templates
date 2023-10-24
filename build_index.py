f33f114ec227439e1f9df62ba777051133f31f55
wh1te909 <dcparsi@gmail.com>
2023-10-24 22:14:30 +0000

    add index

---
+++ build_index.py
@@ -0,0 +1,20 @@
+import json
+from pathlib import Path
+from urllib import parse
+
+
+def main():
+    p = Path("templates/").glob("*.json")
+    files = [i.name for i in p if i.is_file()]
+    index = []
+    for file in files:
+        base_url = "https://raw.githubusercontent.com/amidaware/reporting-templates/master/templates/"
+        temp = {"url": base_url + parse.quote(file)}
+        index.append(temp)
+
+    with Path("index.json").open("w") as fp:
+        json.dump(index, fp)
+
+
+if __name__ == "__main__":
+    main()
