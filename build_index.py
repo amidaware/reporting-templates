d5dcaf6012391c0f101a12a08e1032342df0229b
silversword411 <github.com@datmail.com>
2023-11-01 13:02:08 -0400

    say goodbye to .json in the name field

--- build_index.py
+++ build_index.py
@@ -2,19 +2,19 @@ import json
 from pathlib import Path
 from urllib import parse
 
-
 def main():
     p = Path("templates/").glob("*.json")
     files = [i.name for i in p if i.is_file()]
     index = []
     for file in files:
+        # Remove the ".json" extension from the filename
+        name_without_extension = file.replace(".json", "")
         base_url = "https://raw.githubusercontent.com/amidaware/reporting-templates/master/templates/"
-        temp = {"name": file, "download_url": base_url + parse.quote(file)}
+        temp = {"name": name_without_extension, "download_url": base_url + parse.quote(file)}
         index.append(temp)
 
     with Path("index.json").open("w") as fp:
         json.dump(index, fp)
-
 
 if __name__ == "__main__":
     main()
